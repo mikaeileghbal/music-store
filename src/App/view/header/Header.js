da32d42e639d2946c14bf7cc8e3484f2328a4628
Mikaeil <mikaeileghbal@yahoo.com>
2022-05-01 22:25:14 +0430

    Fix header controls

--- src/App/view/header/Header.js
+++ src/App/view/header/Header.js
@@ -1,7 +1,20 @@
 import { Link } from "react-router-dom";
 import "./header.css";
+import { FaSearch, FaShoppingCart, FaTimes } from "react-icons/fa";
+import { useState } from "react";
 
 export default function Header() {
+  const [toggle, setToggle] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const toggleSearch = (e) => {
+    setToggle(!toggle);
+  };
+
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
   return (
     <header className="App-header">
       <div className="container">
@@ -21,6 +34,34 @@ export default function Header() {
             </li>
           </ul>
         </nav>
+        <div className="button-container">
+          {toggle && (
+            <div className="search-container">
+              <input
+                type="text"
+                value={searchTerm}
+                placeholder="Search site"
+                onChange={handleSearch}
+              />
+              <button
+                type="button"
+                className="button button--header button--search"
+              >
+                <FaSearch />
+              </button>
+            </div>
+          )}
+          <button
+            type="button"
+            className="button button--header"
+            onClick={toggleSearch}
+          >
+            {toggle ? <FaTimes /> : <FaSearch />}
+          </button>
+          <button type="button" className="button button--header">
+            <FaShoppingCart />
+          </button>
+        </div>
       </div>
     </header>
   );
