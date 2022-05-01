feec0079770f38382c4e677d786689b52700952e
Mikaeil Eghbal <mikaeileghbal@yahoo.com>
2022-05-01 09:31:57 +0430

    Create app structure

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Music Store</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
