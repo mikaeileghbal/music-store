c4f68bf99c576e02c09f7dc86320633996016f37
Mikaeil <mikaeileghbal@yahoo.com>
2022-12-14 19:57:00 +0330

    Style basket component

--- src/components/BasketView.js/BasketView.js
+++ src/components/BasketView.js/BasketView.js
@@ -10,6 +10,7 @@ export default function BasketView({ cartData }) {
   return (
     <section className="basket__section">
       <BasketHeader />
+
       <div className="basket__container">
         <BasketProducts cart={cart} />
         <BasketDetails cartItems={cartItems} cartPrice={cartPrice} />
@@ -22,6 +23,20 @@ function BasketHeader() {
   return (
     <div className="basket__header">
       <h2 className="page-title">Basket</h2>
+    </div>
+  );
+}
+
+function BasketBanner() {
+  return (
+    <div className="basket__banner">
+      <p>
+        Total to pay (inc. VAT): <span className="total">$91.20</span>
+      </p>
+      <p className="discount">
+        Free delivery on orders over <span>$20</span>
+      </p>
+      <p className="small">(View delivery options in checkout)</p>
     </div>
   );
 }
@@ -40,23 +55,38 @@ function BasketProducts({ cart }) {
 
   return (
     <section className="basket__products">
+      <BasketBanner />
       <table className="basket__table">
+        <thead>
+          <tr>
+            <th colSpan={3}>Product</th>
+            <th>Qty</th>
+            <th>Subtotal</th>
+          </tr>
+        </thead>
         <tbody>
           {cart.map(({ product, qty }) => (
             <tr key={product.id}>
               <td>
-                <button onClick={() => handleRemove(product)}>
-                  <FaTimes />
+                <button
+                  className="remove__button"
+                  onClick={() => handleRemove(product)}
+                >
+                  <span className="remove__icon">
+                    <FaTimes />
+                  </span>
+                  <span className="remove__text">REMOVE</span>
                 </button>
               </td>
               <td className="image__column">
                 <img src={`images/${product.image}`} alt={product.name} />
               </td>
-              <td>
+              <td className="product__column">
                 <h2>{product.name}</h2>
-                <p>{product.description}</p>
+                <p>
+                  Unit price: <span>${product.price.toFixed(2)}</span>
+                </p>
               </td>
-              <td>{product.price}</td>
               <td>
                 <div className="action__wrap">
                   <input type="text" value={qty} name="qty" />
@@ -68,7 +98,9 @@ function BasketProducts({ cart }) {
                   </button>
                 </div>
               </td>
-              <td>${(product.price * qty).toFixed(2)}</td>
+              <td className="price__column">
+                ${(product.price * qty).toFixed(2)}
+              </td>
             </tr>
           ))}
         </tbody>
