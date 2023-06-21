9f9e2bfccb936018ce27282fdc3f80a8e8bee43d
Mikaeil Eghbal <mikaeileghbal@yahoo.com>
2023-06-21 10:39:08 +0330

    Fix category item and product image aspect ratio

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,10 +1,10 @@
 import React from "react";
-import banner from "../../assets/images/banner.webp";
+import banner from "../../assets/images/banner-2.webp";
 import "./Banner.scss";
 
 export default function Banner() {
   return (
-    <div className="category-hero container">
+    <div className="category-hero">
       <img src={banner} alt="Banner" />
     </div>
   );
