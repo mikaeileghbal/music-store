83a12fafca8631bd18b5667cd84012ce1ba1232a
Mikaeil Eghbal <mikaeileghbal@yahoo.com>
2023-06-21 09:47:52 +0330

    Add transition to notify component

--- src/components/Notification/Notification.js
+++ src/components/Notification/Notification.js
@@ -1,9 +1,22 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import CartNotify from "../CartNotify/CartNotify";
+import { CSSTransition } from "react-transition-group";
+import "./Notification.scss";
 
 export default function Notification() {
   const { showCartNotify } = useSelector((state) => state.stateData);
 
-  return <>{showCartNotify && <CartNotify />}</>;
+  return (
+    <div>
+      <CSSTransition
+        in={showCartNotify}
+        timeout={{ enter: 400, exit: 400 }}
+        classNames="notify-slide"
+        unmountOnExit
+      >
+        <CartNotify />
+      </CSSTransition>
+    </div>
+  );
 }
